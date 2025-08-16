f8a596de215f4f29181c4f32da81b00017d4bf77
warangalonepoint <gullapallychaitanya@gmail.com>
2025-08-16 22:15:04 +0530

    Update sw.js

--- Onestop-demo/clinic/sw.js
+++ Onestop-demo/clinic/sw.js
@@ -1,15 +1,6 @@
 self.addEventListener('install', e=>{
-  e.waitUntil(
-    caches.open('clinic-v1').then(c=>c.addAll([
-      './',
-      './index.html',
-      './styles.css',
-      './assets/clinic_banner.png'
-    ]))
-  );
+  e.waitUntil(caches.open('clinic-v1').then(c=>c.addAll(['./','./index.html','./styles.css'])));
 });
 self.addEventListener('fetch', e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then(res=>res || fetch(e.request)));
 });
