14732527d43687276009703f8c841c40d1473ae6
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-05 09:55:42 +0530

    Update supabase.js

--- school/js/supabase.js
+++ school/js/supabase.js
@@ -1,8 +1,8 @@
 // /js/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "https://YOURPROJECT.supabase.co";
-const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+const SUPABASE_URL = "https://ytlatpgbdpdfqtvktmqo.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0bGF0cGdiZHBkZnF0dmt0bXFvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTcwMzgxODcsImV4cCI6MjA3MjYxNDE4N30.q0isHJP_oKuXeOdRKOq23WcZniBvKt1jbTV7OdgHubE";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
