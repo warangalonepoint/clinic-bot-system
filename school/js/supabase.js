8993bb2039b3bd98dc0bf468c061c6a5e512ef0f
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-05 09:11:19 +0530

    Update supabase.js

--- school/js/supabase.js
+++ school/js/supabase.js
@@ -1,20 +1,16 @@
-<script type="module">
-// /school/js/supabase.js
+// /js/supabase.js
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-// TODO: replace these two:
 const SUPABASE_URL = "https://YOURPROJECT.supabase.co";
 const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
 
 export const sb = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// simple helpers
-export async function getUser() {
-  return (await sb.auth.getUser()).data.user;
-}
 export async function requireLogin() {
-  const u = await getUser();
-  if (!u) location.href = "./login.html";
-  return u;
+  const { data: { user } } = await sb.auth.getUser();
+  if (!user) {
+    location.href = "login.html";
+    throw new Error("Not logged in");
+  }
+  return user;
 }
-</script>
